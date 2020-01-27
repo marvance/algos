e36a26d6136bd8badd5107b5ca0461871e7c87b3
marvance <vancemargot@gmail.com>
2020-01-27 18:58:33 -0500

    fibo

--- projectEuler.js
+++ projectEuler.js
@@ -28,3 +28,26 @@ function multiplesOf3And5(num){
 }
 
 multiplesOf3And5(20)
+
+
+//Sum Even Fibonacci Numbers below num
+function findEvenSum(n){
+
+  let first = 1,
+    second = 2,
+    sum = 2,
+    fibNum;
+
+  if (n<=1) return sum;
+  
+  for (let i=3; i<=n; i++) {
+    fibNum = first + second;
+    first = second;
+    second = fibNum;
+    if (fibNum % 2 == 0) sum += fibNum;
+  }  
+  return sum;
+}
+
+findEvenSum(18)
+
